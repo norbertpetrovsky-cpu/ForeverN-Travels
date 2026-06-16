7625038eef6043966752068108ee7dc7bf7a4eee
norbertpetrovsky-cpu <norbertpetrovsky@gmail.com>
2026-06-16 08:24:05 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import { useLocation } from 'react-router-dom'
+import Landing  from './pages/Landing/Landing'
+import Timeline from './pages/Timeline/Timeline'
+import Trip     from './pages/Trip/Trip'
+import Balenie  from './pages/Balenie/Balenie'
+import Plan     from './pages/Plan/Plan'
+
+export default function App() {
+  const location = useLocation()
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/"                        element={<Landing />} />
+        <Route path="/timeline"                element={<Timeline />} />
+        <Route path="/trip/:id"                element={<Trip />} />
+        <Route path="/trip/:id/balenie"        element={<Balenie />} />
+        <Route path="/trip/:id/plan"           element={<Plan />} />
+        <Route path="/trip/:id/:module"        element={<Trip />} />
+      </Routes>
+    </AnimatePresence>
+  )
+}
